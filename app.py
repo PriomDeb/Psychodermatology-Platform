af4f5f3ac7066aa1715ef0ec30f8c141156a64a3
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2024-10-01 01:50:32 +0600

    Random and user data

--- app.py
+++ app.py
@@ -26,9 +26,13 @@ import streamlit as st
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
+from random_data import prediction_from_user_data
+
 
 file_path = "/Users/priom/Desktop/Psychodermatology"
-# df = pd.read_excel(f"{file_path}/PsyDerm_new_final.xlsx")
+
+def load_df():
+    return pd.read_excel(f"{file_path}/PsyDerm_new_final.xlsx")
 
 def initialize_model(model_path=f"{file_path}/model_01_30-09-2024_00-26-21.keras"):
     try:
@@ -51,53 +55,17 @@ with st.sidebar:
 )
     
 if selected == "Home":
-    col1, col2, col3 = st.columns(3)
-    
-    # Input fields for each feature with corresponding labels
-    with col1:
-        sex = st.number_input('Sex (0 or 1)', min_value=0, max_value=1, value=1)
-        imped_lat = st.number_input('Impedance Latency', min_value=0.0, value=1.5)
-        bas_entertainment_sum = st.number_input('BAS Entertainment Sum', min_value=0, value=5)
-        hq_fear_level_and_sensitivity_left = st.number_input('HQ Fear Level Left', min_value=0, value=3)
-        hq_pair_bonding_and_spouse_dominance_style_right = st.number_input('HQ Pair Bonding Right', min_value=0, value=1)
-    
-    with col2:
-        r_lfa = st.number_input('r_LFA', min_value=0.0, value=0.5)
-        bacq_withdrawal_coping_sum = st.number_input('BACQ Withdrawal Coping Sum', min_value=0, value=4)
-        hq_logical_orientation_right = st.number_input('HQ Logical Orientation Right', min_value=0, value=1)
-        hq_fear_level_and_sensitivity_right = st.number_input('HQ Fear Level Right', min_value=0, value=2)
-        wapi_verbal_left = st.number_input('WAPI Verbal Left', min_value=0, value=4)
-        
-    
-    with col3:
-        pns = st.number_input('PNS', min_value=0.0, value=3.2)
-        bas_drive_sum = st.number_input('BAS Drive Sum', min_value=0, value=2)
-        hq_type_of_consciousness_right = st.number_input('HQ Type of Consciousness Right', min_value=0, value=2)
-        hq_pair_bonding_and_spouse_dominance_style_left = st.number_input('HQ Pair Bonding Left', min_value=0, value=3)
-        rsq_anxious_attachment = st.number_input('RSQ Anxious Attachment', min_value=0, value=2)
-        
-
-    # Example of custom data based on selected features
-    custom_data = np.array([
-        sex, r_lfa, pns, imped_lat, bacq_withdrawal_coping_sum,
-        bas_drive_sum, bas_entertainment_sum, hq_logical_orientation_right,
-        hq_type_of_consciousness_right, hq_fear_level_and_sensitivity_left,
-        hq_fear_level_and_sensitivity_right, hq_pair_bonding_and_spouse_dominance_style_left,
-        hq_pair_bonding_and_spouse_dominance_style_right, wapi_verbal_left, rsq_anxious_attachment
-    ]).reshape(1, -1)
-    
+    tab1, tab2 = st.tabs(["Custom Data", "Get Random Data from Dataset"])
     model = initialize_model()
-    predictions = model.predict(custom_data)
+    df = load_df()
     
-    # Get the predicted class
-    predicted_class = np.argmax(predictions, axis=1)
-    print(f"Predicted class: {predicted_class[0]}")
-    st.write(f"Predicted Class: {predicted_class[0]}")
-        
+    with tab1:
+     prediction_from_user_data(df=df, model=model, keys="tab1")
+    
+    with tab2:
+     prediction_from_user_data(df=df, model=model, keys="tab2", random=True)
 
     
-    
-    pass
 
 if selected == "Stats":
     st.header('Snowflake Healthcare App')
---
+++ random_data.py
@@ -0,0 +1,133 @@
+import streamlit as st
+import streamlit_option_menu
+from streamlit_option_menu import option_menu
+import numpy as np
+
+def prediction_from_user_data(df, model, keys, random=False):
+    col1, col2, col3 = st.columns(3)
+    df.columns = df.columns.str.lower()
+    
+    selected_features = ['sex', 'r_lfa', 'pns', 'imped_lat', 'bacq_withdrawal_coping_sum', 'bas_drive_sum', 'bas_entertainment_sum', 'hq_logical_orientation_right', 'hq_type_of_consciousness_right', 'hq_fear_level_and_sensitivity_left', 'hq_fear_level_and_sensitivity_right', 'hq_pair_bonding_and_spouse_dominance_style_left', 'hq_pair_bonding_and_spouse_dominance_style_right', 'wapi_verbal_left', 'rsq_anxious_attachment']
+    
+    
+    random_row = df.sample(n=1)
+    features = random_row[selected_features].values
+    
+    print(random_row[selected_features + ['group']])
+    
+    columns = [
+    "sex",
+    "r_lfa",
+    "pns",
+    "imped_lat",
+    "bacq_withdrawal_coping_sum",
+    "bas_drive_sum",
+    "bas_entertainment_sum",
+    "hq_logical_orientation_right",
+    "hq_type_of_consciousness_right",
+    "hq_fear_level_and_sensitivity_left",
+    "hq_fear_level_and_sensitivity_right",
+    "hq_pair_bonding_and_spouse_dominance_style_left",
+    "hq_pair_bonding_and_spouse_dominance_style_right",
+    "wapi_verbal_left",
+    "rsq_anxious_attachment"
+    ]
+    
+    values = {col: random_row[col].fillna(0.0).values[0] for col in columns}
+    
+    if not random:
+        values = {col: 0.0 for col in selected_features}
+    
+    sex_value = values["sex"] if random else 1.0
+    r_lfa_value = values["r_lfa"]
+    pns_value = values["pns"]
+    imped_lat_value = values["imped_lat"]
+    bacq_withdrawal_coping_sum_value = values["bacq_withdrawal_coping_sum"]
+    bas_drive_sum_value = values["bas_drive_sum"]
+    bas_entertainment_sum_value = values["bas_entertainment_sum"]
+    hq_logical_orientation_right_value = values["hq_logical_orientation_right"]
+    hq_type_of_consciousness_right_value = values["hq_type_of_consciousness_right"]
+    hq_fear_level_and_sensitivity_left_value = values["hq_fear_level_and_sensitivity_left"]
+    hq_fear_level_and_sensitivity_right_value = values["hq_fear_level_and_sensitivity_right"]
+    hq_pair_bonding_and_spouse_dominance_style_left_value = values["hq_pair_bonding_and_spouse_dominance_style_left"]
+    hq_pair_bonding_and_spouse_dominance_style_right_value = values["hq_pair_bonding_and_spouse_dominance_style_right"]
+    wapi_verbal_left_value = values["wapi_verbal_left"]
+    rsq_anxious_attachment_value = values["rsq_anxious_attachment"]
+    
+    if random:
+        for col in selected_features:
+            if col not in st.session_state:
+                st.session_state[col] = values[col]
+                
+    if random:
+        sex_value = st.session_state["sex"]
+        r_lfa_value = st.session_state["r_lfa"]
+        pns_value = st.session_state["pns"]
+        imped_lat_value = st.session_state["imped_lat"]
+        bacq_withdrawal_coping_sum_value = st.session_state["bacq_withdrawal_coping_sum"]
+        bas_drive_sum_value = st.session_state["bas_drive_sum"]
+        bas_entertainment_sum_value = st.session_state["bas_entertainment_sum"]
+        hq_logical_orientation_right_value = st.session_state["hq_logical_orientation_right"]
+        hq_type_of_consciousness_right_value = st.session_state["hq_type_of_consciousness_right"]
+        hq_fear_level_and_sensitivity_left_value = st.session_state["hq_fear_level_and_sensitivity_left"]
+        hq_fear_level_and_sensitivity_right_value = st.session_state["hq_fear_level_and_sensitivity_right"]
+        hq_pair_bonding_and_spouse_dominance_style_left_value = st.session_state["hq_pair_bonding_and_spouse_dominance_style_left"]
+        hq_pair_bonding_and_spouse_dominance_style_right_value = st.session_state["hq_pair_bonding_and_spouse_dominance_style_right"]
+        wapi_verbal_left_value = st.session_state["wapi_verbal_left"]
+        rsq_anxious_attachment_value = st.session_state["rsq_anxious_attachment"]
+    
+
+    
+    # Input fields for each feature with corresponding labels
+    with col1:
+        sex = st.number_input('Sex (1 or 2)', key=f"{keys}_input_1", min_value=1.0, max_value=2.0, value=sex_value, step=1.0)
+        imped_lat = st.number_input('Impedance Latency', key=f"{keys}_input_2", min_value=-5.0, value=imped_lat_value, format="%.10f")
+        bas_entertainment_sum = st.number_input('BAS Entertainment Sum', key=f"{keys}_input_3", min_value=-50.0, max_value=50.0,   value=bas_entertainment_sum_value)
+        hq_fear_level_and_sensitivity_left = st.number_input('HQ Fear Level Left', key=f"{keys}_input_4", min_value=-10.0, max_value=10.0,     value=hq_fear_level_and_sensitivity_left_value)
+        hq_pair_bonding_and_spouse_dominance_style_right = st.number_input('HQ Pair Bonding Right', key=f"{keys}_input_5", min_value=-10.0,    max_value=10.0, value=hq_pair_bonding_and_spouse_dominance_style_right_value)
+    
+    with col2:
+        r_lfa = st.number_input('r_LFA', key=f"{keys}_input_6", min_value=-5.0, max_value=5.0, value=r_lfa_value, format="%.10f")
+        bacq_withdrawal_coping_sum = st.number_input('BACQ Withdrawal Coping Sum', key=f"{keys}_input_7", min_value=-50.0, max_value=50.0,     value=bacq_withdrawal_coping_sum_value)
+        hq_logical_orientation_right = st.number_input('HQ Logical Orientation Right', key=f"{keys}_input_8", min_value=-5.0,  value=hq_logical_orientation_right_value)
+        hq_fear_level_and_sensitivity_right = st.number_input('HQ Fear Level Right', key=f"{keys}_input_9", min_value=-10.0, max_value=10.0,   value=hq_fear_level_and_sensitivity_right_value)
+        wapi_verbal_left = st.number_input('WAPI Verbal Left', key=f"{keys}_input_10", min_value=-20.0, max_value=20.0,     value=wapi_verbal_left_value)
+        
+    
+    with col3:
+        pns = st.number_input('PNS', key=f"{keys}_input_11", min_value=-10.0, max_value=10.0, value=pns_value)
+        bas_drive_sum = st.number_input('BAS Drive Sum', key=f"{keys}_input_12", min_value=-50.0, max_value=50.0, value=bas_drive_sum_value)
+        hq_type_of_consciousness_right = st.number_input('HQ Type of Consciousness Right', key=f"{keys}_input_13", min_value=-10.0,     max_value=10.0, value=hq_type_of_consciousness_right_value)
+        hq_pair_bonding_and_spouse_dominance_style_left = st.number_input('HQ Pair Bonding Left', key=f"{keys}_input_14", min_value=-10.0,  max_value=10.0, value=hq_pair_bonding_and_spouse_dominance_style_left_value)
+        rsq_anxious_attachment = st.number_input('RSQ Anxious Attachment', key=f"{keys}_input_15", min_value=-50.0, max_value=50.0,     value=rsq_anxious_attachment_value)
+    
+    
+    custom_data = np.array([
+        sex, r_lfa, pns, imped_lat, bacq_withdrawal_coping_sum,
+        bas_drive_sum, bas_entertainment_sum, hq_logical_orientation_right,
+        hq_type_of_consciousness_right, hq_fear_level_and_sensitivity_left,
+        hq_fear_level_and_sensitivity_right, hq_pair_bonding_and_spouse_dominance_style_left,
+        hq_pair_bonding_and_spouse_dominance_style_right, wapi_verbal_left, rsq_anxious_attachment
+    ]).reshape(1, -1)
+
+    
+    
+    if st.button('Run Prediction', key=f"{keys}_random_data_button"):
+        # model = initialize_model()
+        predictions = model.predict(custom_data)
+        predicted_class = np.argmax(predictions, axis=1)
+        st.write(f"Predicted Class: {predicted_class[0]}")
+        
+        # for col in selected_features:
+        #     if col not in st.session_state:
+        #         st.session_state[col] = custom_data[0][selected_features.index(col)]
+    
+    if st.button("Refresh", key=f"{keys}_refresh"):
+        for col in selected_features:
+            del st.session_state[col]
+        
+        st.rerun()
+
+
+
+
